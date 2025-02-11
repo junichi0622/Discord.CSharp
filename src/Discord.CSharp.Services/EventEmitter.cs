5372a61b6d1ca91284bc94126d039838dabd4f54
junichi0622 <j0622s0622@icloud.com>
2025-02-12 03:25:37 +0900

    Add files via upload

--- src/Discord.CSharp.Services/EventEmitter.cs
+++ src/Discord.CSharp.Services/EventEmitter.cs
@@ -1,106 +1,30 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 namespace Discord.CSharp.Services
 {
     public class EventEmitter
     {
-        private readonly Dictionary<string, List<Action<object?>>> _eventHandlers = new();
+        private readonly Dictionary<string, List<Action>> _events = new();
 
-        public void On(string eventName, Action<object?> handler)
+        public void On(string eventName, Action listener)
         {
-            if (!_eventHandlers.ContainsKey(eventName))
+            if (!_events.ContainsKey(eventName))
             {
-                _eventHandlers[eventName] = new List<Action<object?>>();
+                _events[eventName] = new List<Action>();
             }
-            _eventHandlers[eventName].Add(handler);
+            _events[eventName].Add(listener);
         }
 
-        public void Once(string eventName, Action<object?> handler)
+        public void Emit(string eventName)
         {
-            Action<object?> wrappedHandler = null!;
-            wrappedHandler = (data) =>
+            if (_events.ContainsKey(eventName))
             {
-                Off(eventName, wrappedHandler);
-                handler(data);
-            };
-            On(eventName, wrappedHandler);
-        }
-
-        public void Off(string eventName, Action<object?> handler)
-        {
-            if (_eventHandlers.ContainsKey(eventName))
-            {
-                _eventHandlers[eventName].Remove(handler);
-                if (_eventHandlers[eventName].Count == 0)
+                foreach (var listener in _events[eventName])
                 {
-                    _eventHandlers.Remove(eventName);
+                    listener.Invoke();
                 }
             }
-        }
-
-        public void Emit(string eventName, object? data = null)
-        {
-            if (_eventHandlers.ContainsKey(eventName))
-            {
-                foreach (var handler in _eventHandlers[eventName])
-                {
-                    handler.Invoke(data);
-                }
-            }
-        }
-
-        public int ListenerCount(string eventName)
-        {
-            return _eventHandlers.ContainsKey(eventName) ? _eventHandlers[eventName].Count : 0;
-        }
-
-        public List<Action<object?>>? Listeners(string eventName)
-        {
-            return _eventHandlers.ContainsKey(eventName) ? new (_eventHandlers[eventName]) : null;
-        }
-
-        public void RemoveAllListeners(string? eventName = null)
-        {
-            if (eventName == null)
-            {
-                _eventHandlers.Clear();
-            }
-            else
-            {
-                _eventHandlers.Remove(eventName);
-            }
-        }
-
-        public List<string> EventNames()
-        {
-            return new List<string>(_eventHandlers.Keys);
-        }
-
-        public void PrependListener(string eventName, Action<object?> handler)
-        {
-            if (!_eventHandlers.ContainsKey(eventName))
-            {
-                _eventHandlers[eventName] = new List<Action<object?>>();
-            }
-            _eventHandlers[eventName].Insert(0, handler);
-        }
-
-        public void PrependOnceListener(string eventName, Action<object?> handler)
-        {
-            Action<object?> wrappedHandler = null!;
-            wrappedHandler = (data) =>
-            {
-                Off(eventName, wrappedHandler);
-                handler(data);
-            };
-            
-            if (!_eventHandlers.ContainsKey(eventName))
-            {
-                _eventHandlers[eventName] = new List<Action<object?>>();
-            }
-            _eventHandlers[eventName].Insert(0, wrappedHandler);
         }
     }
 }
